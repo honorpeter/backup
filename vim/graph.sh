44c70bca4c3c6c0817c91db5505a2859e656471f
Your Name <you@example.com>
2015-05-06 22:59:57 +0800

    add config digital

---
+++ vim/graph.sh
@@ -0,0 +1,23 @@
+插入特殊字符：
+    :help digraphs 
+        to see how they're used 
+    :digraphs to list the defined ones in your Vim.
+        常用的：
+            ϝ   m3
+            Ϝ   M3
+            ∴ is S
+            ≠ is !
+            ∑ is +
+            ≡ is =
+            ⇐ is <=
+            ⇒ is =
+            ⇔ is =
+            ∀ is F
+            ∃ is T
+            ∋ is -
+            ¯ is 'm
+            ·is .M 
+            
+
+ 
+    :set ft=mathdoc
