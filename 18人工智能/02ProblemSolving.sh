b715d9e83b0dfdb630ab86ac2a306564dea8ad50
xiaolong <jixiaolong888@gmail.com>
2015-08-30 21:06:37 +0800

    update

---
+++ 18人工智能/02ProblemSolving.sh
@@ -0,0 +1,25 @@
+Solving Problems by Uninformed Searching:
+    不去预估
+        Breadth-First
+        Uniform-Cost
+        Depth_Limited
+        iterative Deepening
+        Birdrectional
+    
+    Formulate goal
+    Formulate problem
+        States: 
+        Actions
+
+        initial state
+        Actions;
+        Transition model
+        Goal test
+        Path cost
+    Find solution
+        Sequence of actions
+        A solution is a sequence of actions leading from the initial state
+            to a goal state
+Solving Problems by informed Searching:
+
+        
