b54753a110af63fc502b12aab9658f8f4a74aa68
Your Name <you@example.com>
2015-09-04 17:09:13 +0800

    update

--- 18人工智能/02ProblemSolving.sh
+++ 18人工智能/02ProblemSolving.sh
@@ -3,7 +3,10 @@ Solving Problems by Uninformed Searching:
         Breadth-First
         Uniform-Cost
         Depth_Limited
+            使用深度来限制
+            使用path cost来左限制
         iterative Deepening
+            使用深度来限制     深度搜索
         Birdrectional
     
     Formulate goal
@@ -21,5 +24,21 @@ Solving Problems by Uninformed Searching:
         A solution is a sequence of actions leading from the initial state
             to a goal state
 Solving Problems by informed Searching:
+        Tree Search Algorithms:
+            把每个状态 连接成一个树形结构  
+            这样有重复的节点 
+            
+            可以使用hash table 相同的hash值 当做同一个
 
-        
+            也可以 访问过的标记 避免重复
+
+
+
+
+
+
+
+
+
+
+
---
+++ 18人工智能/03informedSearch.sh
@@ -0,0 +1,17 @@
+informed Search  有预估的搜索
+
+    Best-First Search
+
+    Memory Bounded Search
+
+    Heuristic
+        
+
+
+
+
+
+
+
+
+
