ef8707d5ab5751d8002207e2e6f2091858bade2c
Your Name <you@example.com>
2016-02-26 23:17:59 +0800

    update

---
+++ 20R语言/01RBasics.sh
@@ -0,0 +1,50 @@
+install and update
+    install:
+            linux:  sudo apt-get install R.x86_64
+
+    Update:
+            linxu:  sudo apt-get update  R.x86_64
+
+
+
+
+    Open:
+            command: R
+            The interface for R:
+                command: R -g Tk &   
+
+    The R Console:
+            like the ubuntu terminal
+
+    Command-Line Editing:
+            edit()  
+    Batch Mode:
+            R CMD BATCH generate_graphs.R       
+                running linux terminal
+                generating an output file called generate_graphs that rename
+                        R CMD BATCH generate_graphs.R generate_graphs_`date "+%y%m%d"`.log
+            RScript generate_graphs.R 
+                output to stdin
+
+    Using R Inside Microsoft Excel:
+            ....
+    Other Ways to Run R:
+            As a web application:
+                http://biostat.mc.vanderbilt.edu/rapache/
+            As a server:
+                http://www.rforge.net/Rserve/index.html.
+            Inside Emacs:
+                http://ess.rproject.org/.
+
+A Short R Tutorial:
+    
+
+
+        
+
+
+
+
+
+
+
