a2900d18eb8aef460298473bd402f509affe14e3
Your Name <you@example.com>
2016-07-24 15:46:24 +0800

    update

--- 28happy/01intro.sh
+++ 28happy/01intro.sh
@@ -3,6 +3,26 @@ Positive psychology:
 Silence:
     反思 回忆一下 刚刚的学习 
     说白了就是 自己思考一下 经过自己的大脑
+
+good question：
+    积极的提问 看到自己的优势 看到别人的优势
+    不要一直看到不足 一直关注于不足 你会更加抑郁
+    欣赏优点也会增值
+    欣赏缺点也会变得更加。。。
+
+    Positive question  ask:
+        为什么这么多孩子堕落 ？ 不积极的问法
+        那些人是怎么取得成功的  
+            为什么有一些可以成功即使成长环境恶劣
+    
+illness model:
+    If wo take away the illness, then we become healthy.
+Positive mode: healthy model
+    You are ill because you don't have enough health in your life.
+    because you are not prsuing those things that make you healthy.
+        What make you healthy? pursuing meaning, purpose; 
+                            cultivating healthy relationships
+Stop blaming others, Take responsibility for your life.
     
 
 
